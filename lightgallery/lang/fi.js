da2a60e6a6e7296695277e8115253ff0894efc52
Dmitri Ischenko <ischenkodv@gmail.com>
2015-02-27 23:23:25 +0200

    Added finnish translations

---
+++ lightgallery/lang/fi.js
@@ -0,0 +1,13 @@
+try{
+    lightgallery.setLangVars({
+        next	: 'Seuraava',
+        prev	: 'Edellinen',
+        zoomIn	: 'Suurenna',
+        zoomOut	: 'Loitonna',
+        fullSize: 'Koko näyttö',
+        fitScreen: 'Sovita ruudulle',
+        close	: 'Sulje',
+        image	: 'Kuva',
+        of		: '/'
+    })
+}catch(e){}
